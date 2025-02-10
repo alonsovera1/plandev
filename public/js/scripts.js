59926371d71197ef2b6ffdcbda5ab6667eb1f1fa
Alonso <alonsovera0101@gmail.com>
2025-02-09 20:40:51 -0400

    primer commit

---
+++ public/js/scripts.js
@@ -0,0 +1,23 @@
+document.addEventListener('DOMContentLoaded', (event) => {
+    console.log('DOM fully loaded and parsed');
+    
+    // Manejo de cookies
+    const cookieBanner = document.createElement('div');
+    cookieBanner.innerHTML = `
+        <div class="cookie-banner">
+            <p>Este sitio usa cookies. ¿Aceptar?</p>
+            <button id="accept-cookies">Aceptar</button>
+            <button id="decline-cookies">Rechazar</button>
+        </div>`;
+    document.body.appendChild(cookieBanner);
+
+    document.getElementById('accept-cookies').addEventListener('click', () => {
+        document.cookie = "userAcceptedCookies=true; path=/";
+        cookieBanner.style.display = 'none';
+    });
+
+    document.getElementById('decline-cookies').addEventListener('click', () => {
+        document.cookie = "userAcceptedCookies=false; path=/";
+        cookieBanner.style.display = 'none';
+    });
+});
